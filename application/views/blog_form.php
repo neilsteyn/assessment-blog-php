e9792de8f2f4761f1c7c1f9c690d51cca46627aa
Neil Steyn <neiljsteyn@gmail.com>
2021-09-01 09:13:27 +0200

    Fixed add blog form

--- application/views/blog_form.php
+++ application/views/blog_form.php
@@ -6,9 +6,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <?php echo validation_errors(); ?>
 
 	<form method="POST" action="">
-		<input type="text" name="title" placeholder="Title" value="<?= $data->title; ?>">
-		<input type="text" name="description" placeholder="Description" value="<?= $data->description; ?>">
-		<input type="text" name="content" placeholder="Content" value="<?= $data->content; ?>">
+		<input type="text" name="title" placeholder="Title" value="<?= (isset($data->title) ? $data->title : ''); ?>">
+		<input type="text" name="description" placeholder="Description" value="<?= (isset($data->description) ? $data->description : ''); ?>">
+		<input type="text" name="content" placeholder="Content" value="<?= (isset($data->content) ? $data->content : ''); ?>">
 		<button type="submit">Create</button>
 	</form>
 </section>
